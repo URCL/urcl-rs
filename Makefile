115c0bff68db861faf51a28ca10d550237c4cc71
emm312 <emmtbm55@gmail.com>
2022-12-11 09:53:14 +1100

    add thing for gh pages

--- Makefile
+++ Makefile
@@ -2,9 +2,13 @@ run:
 	wasm-pack build --target web
 	mv ./pkg/urcl_rs_bg.wasm ./pkg/urcl_rs_tmp.wasm
 	wasm-opt -O3 -o ./pkg/urcl_rs_bg.wasm ./pkg/urcl_rs_tmp.wasm
+	rm ./pkg/.gitignore
+	rm ./pkg/urcl_rs_tmp.wasm
 	python3 -m http.server
 
 build:
 	wasm-pack build --target web
 	mv ./pkg/urcl_rs_bg.wasm ./pkg/urcl_rs_tmp.wasm
 	wasm-opt -O3 -o ./pkg/urcl_rs_bg.wasm ./pkg/urcl_rs_tmp.wasm
+	rm ./pkg/.gitignore
+	rm ./pkg/urcl_rs_tmp.wasm
---
+++ pkg/urcl_rs.d.ts
@@ -0,0 +1,41 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+*/
+export function init_panic_hook(): void;
+/**
+* @param {string} src
+*/
+export function emulate(src: string): void;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly init_panic_hook: () => void;
+  readonly emulate: (a: number, b: number) => void;
+  readonly __wbindgen_free: (a: number, b: number) => void;
+  readonly __wbindgen_malloc: (a: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number) => number;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {SyncInitInput} module
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
