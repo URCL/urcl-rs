63dce6f514b4918ead4bf5a05cbc9eb58bfe3e39
Bram <59796107+BramOtte@users.noreply.github.com>
2022-12-11 09:32:32 +0100

    remove build files

--- pkg/urcl_rs_bg.wasm.d.ts
+++
@@ -1,8 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export const memory: WebAssembly.Memory;
-export function emulate(a: number, b: number): void;
-export function init_panic_hook(): void;
-export function __wbindgen_free(a: number, b: number): void;
-export function __wbindgen_malloc(a: number): number;
-export function __wbindgen_realloc(a: number, b: number, c: number): number;
