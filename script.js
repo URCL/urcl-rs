1cc8481099a06676b05045a2254d080d85e6361f
funnsam <funnsam8@gmail.com>
2022-12-15 18:24:37 +0800

    a lot of cool new stuffs

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function by_id(clazz, name) {
 const stdout = by_id(HTMLElement, "stdout");
 const highlight = by_id(HTMLElement, "highlight");
 const code_input = by_id(HTMLTextAreaElement, "code_input");
-const auto_emulate = by_id(HTMLInputElement, "auto-emulate");
+const auto_emulate = by_id(HTMLInputElement, "auto_emulate");
 
 export function now() {
     return performance.now();
@@ -82,10 +82,14 @@ export function resync_highlight() {
     highlight.style.height   = (code_input.getBoundingClientRect().height - parseFloat(getComputedStyle(highlight).fontSize)) + "px";
 }
 
+export function update_debug_buttons(new_state) {
+    for (let i = 0; i < document.getElementsByClassName("debug_only").length; i++) {
+        document.getElementsByClassName("debug_only")[i].style.display = new_state ? "initial" : "none";
+    }
+}
+
 init().then(() => { // all code should go in here
     init_panic_hook();
-    
-    code_input.oninput = e => { output_highlight_span(code_input.value); }
     
     code_input.onkeydown = e => {
         if (e.key == 'Tab') {
@@ -93,27 +97,18 @@ init().then(() => { // all code should go in here
             let a = code_input.selectionStart+1;
             code_input.value = code_input.value.substring(0, code_input.selectionStart) + "\t" + code_input.value.substring(code_input.selectionEnd);
             code_input.setSelectionRange(a, a);
-            output_highlight_span(code_input.value);
-        }
+        };
     };
 
-    code_input.onscroll = e => {
-        highlight.scrollTo(0, code_input.scrollTop);
-    };
+    code_input.oninput                                  = () => output_highlight_span(code_input.value);
+    code_input.onscroll                                 = () => highlight.scrollTo(0, code_input.scrollTop);
+    document.getElementById("document_link").onclick    = () => window.open("https://github.com/ModPunchtree/URCL/releases/latest", "_blank");
+    document.getElementById("emulate").onclick          = () => emulate(code_input.value);
+    document.getElementById("clear").onclick            = () => clear_text();
+    document.getElementById("debug_option").onchange    = () => update_debug_buttons(this.checked);
+    document.getElementById("tab_size").onchange        = () => document.querySelector(":root").style.setProperty("--tab-size", this.value);
+    document.getElementsByTagName("body")[0].onresize   = () => resync_highlight();
 
-    resync_highlight();
-    
-    document.getElementById("emulate").onclick = function() {
-        emulate(code_input.value);
-    };
-
-    document.getElementById("document_link").onclick = function() {
-        window.open("https://github.com/ModPunchtree/URCL/releases/latest", "_blank");
-    };
-
-    document.getElementById("clear").onclick = function() {
-        clear_text();
-    };
 
     document.getElementById("settings").onclick = function() {
         document.getElementById("settings_sec").style.opacity       = 1;
@@ -128,15 +123,46 @@ init().then(() => { // all code should go in here
             document.getElementById("settings_sec").style.pointerEvents = "none";
         }, 250);
     };
+    
+    document.getElementById("examples").onclick = function() {
+        document.getElementById("example_sec").style.opacity       = 1;
+        document.getElementById("example_sec").style.zIndex        = 999;
+        document.getElementById("example_sec").style.pointerEvents = "auto";
+    };
+
+    document.getElementById("exit_examples").onclick = function() {
+        document.getElementById("example_sec").style.opacity           = 0;
+        setTimeout(() => {
+            document.getElementById("example_sec").style.zIndex        = -999;
+            document.getElementById("example_sec").style.pointerEvents = "none";
+        }, 250);
+    };
 
     document.getElementsByTagName("body")[0].onbeforeunload = function() {
         localStorage.setItem("auto_emulate", auto_emulate.checked ? "t" : "f");
+        localStorage.setItem("tab_size", document.getElementById("tab_size").value);
+        localStorage.setItem("debug_option", document.getElementById("debug_option").checked ? "t" : "f");
     };
 
-    document.getElementsByTagName("body")[0].onresize = function() {
-        resync_highlight();
+    for (let i = 0; i < document.getElementsByClassName("example_link").length; i++) {
+        document.getElementsByClassName("example_link")[i].onclick = () => location = this.dataset["link"];
     };
 
-    auto_emulate.checked = localStorage.getItem("auto_emulate") == "t" ? true : false;
+    auto_emulate.checked = localStorage.getItem("auto_emulate") != "f";
+
+    document.getElementById("tab_size").value = localStorage.getItem("tab_size") == null ? 4 : localStorage.getItem("tab_size");
+    document.querySelector(":root").style.setProperty("--tab-size", document.getElementById("tab_size").value);
+    
+    document.getElementById("debug_option").checked = localStorage.getItem("debug_option") == "t";
+    update_debug_buttons(document.getElementById("debug_option").checked);
+
+    resync_highlight();
     output_highlight_span(code_input.value);
+
+    const params = new URLSearchParams(window.location.search);
+
+    if (params.has("from-examples")) {
+        alert("Examples are not done yet!");
+        window.history.replaceState("", "urcl-rs", location.href.replace(window.location.search, ""));
+    };
 });
