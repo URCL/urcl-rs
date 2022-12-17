538390cc32d459b2d69355ba6bb328e8cc339cd4
funnsam <61924648+funnsam@users.noreply.github.com>
2022-12-17 22:53:21 +0800

    oof

--- src/emulator/ast.rs
+++ src/emulator/ast.rs
@@ -153,7 +153,7 @@ pub fn gen_ast<'a>(toks: Vec<UToken<'a>>) -> Program {
                         p.buf.advance();
                     },
                     "bits" => {
-                        match p.buf.next().kind { Kind::GE | Kind::LE | Kind::Eq, _ => p.buf.advance(), _ => () };
+                        match p.buf.next().kind { Kind::GE | Kind::LE | Kind::Eq => p.buf.advance(), _ => () };
                         p.ast.headers.bits = match p.buf.next().kind {Kind::Int(v) => v as u64, _ => {continue;}};
                         p.buf.advance();
                     },
