e350da9103e73f308829297f8bdf5e43d879d44c
funnsam <funnsam8@gmail.com>
2022-12-23 17:09:06 +0800

    added cal and ret

--- src/emulator/emulator.rs
+++ src/emulator/emulator.rs
@@ -403,6 +403,8 @@ impl EmulatorState  {
             BSL(a, b, c) => self.set(a, self.get(b)>>self.get(c)),
             SRS(a, b) => self.set(a, ((self.get(b) as i64) >> 1) as u64),
             BSS(a, b, c) => self.set(a, ((self.get(b) as i64) >> (self.get(c) as i64)) as u64),
+            CAL(a) => {self.stack_push(self.get(a)); self.pc = self.get(a) as usize - 1},
+            RET => self.pc = self.stack_pop() as usize,
             _ => jsprintln!("Unimplimented instruction."),
         }
         self.pc += 1;
