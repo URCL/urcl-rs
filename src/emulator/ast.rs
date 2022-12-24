033d210ad42e0671cd572ce9ba05007da61941ee
funnsam <61924648+funnsam@users.noreply.github.com>
2022-12-24 12:47:21 +0800

    ???

--- src/emulator/ast.rs
+++ src/emulator/ast.rs
@@ -349,9 +349,7 @@ impl <'a> Parser<'a> {
                         if !matches!(self.buf.next().kind, Kind::Char) {
                             self.err.error(&self.buf.current(), ErrorKind::EOFBeforeEndOfString);
                         }
-                        let a = AstOp::Char(self.buf.next().str.chars().next().unwrap());
-                        self.buf.next();
-                        a
+                        AstOp::Char(self.buf.current().str.chars().next().unwrap())
                     }
                     Kind::Escape(c) => {
                         if !matches!(self.buf.next().kind, Kind::Char) {
